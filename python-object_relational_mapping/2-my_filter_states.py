2add82226ad5e6c62f37ed758bd51dc2b270fe5b
Ae7is <aejis777@gmail.com>
2026-08-15 15:01:04 +0100

    This script takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -14,9 +14,8 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(
-        sys.argv[4]
-    )
+    query = "SELECT * FROM states WHERE BINARY name = '{}' " \
+            "ORDER BY id ASC".format(sys.argv[4])
     cur.execute(query)
 
     for state in cur.fetchall():
